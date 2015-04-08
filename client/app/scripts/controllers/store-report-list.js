b98e368914c2297e04044412572aa4c6e93ccdb6
Pulkit Singhal <pulkit@shoppinpal.com>
2015-04-08 15:00:52 -0500

    update mock data

--- client/app/scripts/controllers/store-report-list.js
+++ client/app/scripts/controllers/store-report-list.js
@@ -12,8 +12,12 @@ angular.module('ShoppinPalApp')
     '$scope', '$state',
     function ($scope, $state)
     {
-      $scope.reportList = ['GlenStone','Skelly','ParkWay','kellog'];
-
+      $scope.reportList = [
+        'GlenStone - Mon 5th Apr 2015',
+        'GlenStone - Fri 9th Jun 2015',
+        'GlenStone - Tue 20th Nov 2015',
+        'GlenStone - Wed 21st Nov 2015'
+      ];
 
       /** @method generateOrder
        * This method will move to generate store report for particular store
