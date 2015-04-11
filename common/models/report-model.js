f45006c3aba7d55310edbbbbf8010d3f474adbf7
Pulkit Singhal <pulkit@shoppinpal.com>
2015-04-10 20:55:41 -0500

    submit work to IronWorker

--- common/models/report-model.js
+++ common/models/report-model.js
@@ -84,61 +84,83 @@ module.exports = function(ReportModel) {
 
   ReportModel.generateStockOrderReportForManager = function(id, cb) {
     var currentUser = getCurrentUserModel(cb); // returns  immediately if no currentUser
-
     if(currentUser) {
-      // (1) fetch the reportModelInstance in question
-      ReportModel.findByIdAsync(
-        //{filter:{where: {id: id}}}
-        //{id: id}
-        id
-      )
-        .then(function(reportModelInstance) {
-          log('print object for reportModelInstance: ', reportModelInstance);
+      // (1) generate a token for the worker to use on the currentUser's behalf
+      currentUser.createAccessTokenAsync(1209600)// can't be empty ... time to live (in seconds) 1209600 is 2 weeks (default of loopback)
+        .then(function(newAccessToken){
+          // (2) fetch the report
+          return ReportModel.findByIdAsync(id) // chain the promise via a return statement so unexpected rejections/errors float up
+            .then(function(reportModelInstance) {
+              log('print object for reportModelInstance: ', reportModelInstance);
+              // (3) fetch the store
+              // TODO: is findOne buggy? does it return a result even when there are no matches?
+              return ReportModel.app.models.StoreModel.findOneAsync( // chain the promise via a return statement so unexpected rejections/errors float up
+                {
+                  where: {'api_id': reportModelInstance.outlet.id}, //assumption: there aren't any duplicate entries
+                  include: 'storeConfigModel' // (4) also fetch the store-config
+                }
+              )
+                .then(function(storeModelInstance){
+                  log('print object for storeModelInstance: ', storeModelInstance);
 
-          // TODO: (2) get the config settings for calling iron.io
-          log('ReportModel.app.get(\'site:baseUrl\')', ReportModel.app.get('site:baseUrl'));
-          log('process.env[\'site:baseUrl\']', process.env['site:baseUrl']);
+                  // (5) extract domainPrefix from store-config's posUrl
+                  var posUrl = storeModelInstance.storeConfigModel().posUrl;
+                  var regexp = /^https?:\/\/(.*)\.vendhq\.com$/i;
+                  var matches = posUrl.match(regexp);
 
-          // TODO: (3) generate a token for the worker to use on the currentUser's behalf
-          // TODO: (4) trigger a rest call to iron.io w/ arguments from reportModelInstance
-          var options = {
-            url: ReportModel.app.get('ironWorkersUrl'),
-            qs: {
-              'oauth': ReportModel.app.get('ironWorkersOauthToken'),
-              // TODO: deploy a worker in iron.io
-              //       error: 'No code found to run for code.name = loopback.development'
-              'code_name': ReportModel.app.get('stockOrderWorker'),
-              'priority': 1
-            },
-            json: {
-              'token': '<empty>',
-              'outletId': reportModelInstance.outlet.id,
-              'supplierId': reportModelInstance.supplier.id
-            }
-          };
-          log('options', JSON.stringify(options,null,2));
-          return request.post(options)
-            .then(successHandler)
-            .then(function(data){
-              log('data', JSON.stringify(data,null,2));
-              cb(null, data);
-            })
-            .catch(ClientError, function(e) {
-              var message = e.response.body;
-              if(_.isObject(message)) {
-                message = JSON.stringify(message,null,2);
-              }
-              console.error('A ClientError happened: \n'
-                  + e.statusCode + ' ' + message + '\n'
-                  /*+ JSON.stringify(e.response.headers,null,2)
-                  + JSON.stringify(e,null,2)*/
-              );
-              // TODO: add retry logic?
-              return Promise.reject(e.statusCode + ' ' + message); // TODO: throw unknown errors but reject well known errors?
-            })
-            .catch(function(e) {
-              console.error('report-model.js - generateStockOrderReportForManager - An unexpected error occurred: ', e);
-              throw e; // TODO: throw unknown errors but reject well known errors?
+                  // (6) Prepare payload for worker
+                  var options = {
+                    url: ReportModel.app.get('ironWorkersUrl'),
+                    qs: {
+                      'oauth': ReportModel.app.get('ironWorkersOauthToken'),
+                      'code_name': ReportModel.app.get('stockOrderWorker'),
+                      'priority': 1
+                    },
+                    json: {
+                      tokenService: 'https://{DOMAIN_PREFIX}.vendhq.com/api/1.0/token', //TODO: fetch from global-config or config.*.json
+                      clientId: ReportModel.app.get('vend').client_id,
+                      clientSecret: ReportModel.app.get('vend').client_secret,
+                      tokenType: 'Bearer',
+                      accessToken: storeModelInstance.storeConfigModel().vendAccessToken,//'XN4ceup1M9Rp6Sf1AqeqarDjN9TMa06Mwr15K7lk',
+                      refreshToken: storeModelInstance.storeConfigModel().vendRefreshToken,//'qSl8JF9fD2UMGAZfpsN2yr2d8XRNZgmQEKh7v5jp',
+                      domainPrefix: matches[1], //'fermiyontest', // TODO: extract from storeConfigModelInstance.posUrl
+                      loopbackServerUrl: process.env['site:baseUrl'],
+                      //loopbackServerHost: 'mppulkit1.localtunnel.me',
+                      //loopbackServerPort: '443',
+                      loopbackAccessToken: newAccessToken.id,//'cWSlf731lpFFbkL1Qt6cnIAb9c8QdKFunnvFGf87Xrh93ottgA8aONuHA1Vt42Kw',
+                      reportId: id,
+                      outletId: reportModelInstance.outlet.id,//'aea67e1a-b85c-11e2-a415-bc764e10976c',
+                      supplierId: reportModelInstance.supplier.id//'c364c506-f8f4-11e3-a0f5-b8ca3a64f8f4'
+                    }
+                  };
+                  log('options', JSON.stringify(options,null,2));
+                  request.post(options)
+                    .then(successHandler)
+                    .then(function(data){
+                      log('data', JSON.stringify(data,null,2));
+                      cb(null, data);
+                    })
+                    .catch(ClientError, function(e) {
+                      var message = e.response.body;
+                      if(_.isObject(message)) {
+                        message = JSON.stringify(message,null,2);
+                      }
+                      console.error('A ClientError happened: \n'
+                          + e.statusCode + ' ' + message + '\n'
+                        /*+ JSON.stringify(e.response.headers,null,2)
+                         + JSON.stringify(e,null,2)*/
+                      );
+                      // TODO: add retry logic?
+                      //return Promise.reject(e.statusCode + ' ' + message); // TODO: throw unknown errors but reject well known errors?
+                      cb(e.statusCode + ' ' + message);
+                    })
+                    .catch(function(e) {
+                      console.error('report-model.js - generateStockOrderReportForManager - An unexpected error occurred: ', e);
+                      //throw e; // TODO: throw unknown errors but reject well known errors?
+                      //return Promise.reject(e);
+                      cb(e);
+                    });
+                });
             });
         },
         function(error){
