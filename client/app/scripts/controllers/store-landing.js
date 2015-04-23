28ae1c36fa20bc437283786888cb25c2f752b315
chhaya <chhaya.rai@planetria.com>
2015-04-23 18:26:46 +0530

    content scroll given for store-landing page

--- client/app/scripts/controllers/store-landing.js
+++ client/app/scripts/controllers/store-landing.js
@@ -8,8 +8,21 @@
  * Controller of the ShoppinPalApp
  */
 angular.module('ShoppinPalApp')
-  .controller('StoreLandingCtrl',[
-    '$scope',
-    function ($scope){
+  .controller('StoreLandingCtrl',['$scope','loginService',
+  	function ($scope,loginService){
+
+  		/** @method viewContentLoaded
+       * This method will load the storesReport from api on view load
+       */
+      $scope.$on('$viewContentLoaded', function() {
+        //loginService.getSelectStore()
+        loginService.getSelectStore()
+          .then(function (response) {
+            $scope.storesReport = response.data.storesReport;
+            $scope.storereportlength = $scope.storesReport.length;
+           // $scope.JumtoDepartment();
+          });
+      });
+
     }
     ]);	
