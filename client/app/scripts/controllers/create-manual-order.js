0af731508afe6cf4f180bb921d8ac48815844535
Pulkit Singhal <pulkit@shoppinpal.com>
2015-06-02 12:13:09 -0500

    resolved ShoppinPal/warehouse issue #70
    ... only the stores which the current user is the manager for are retrieved

--- client/app/scripts/controllers/create-manual-order.js
+++ client/app/scripts/controllers/create-manual-order.js
@@ -8,8 +8,13 @@
  * Controller of the ShoppinPalApp
  */
 angular.module('ShoppinPalApp')
-    .controller('createManualOrderCtrl', ['$scope','SupplierModel', function($scope,SupplierModel) {
-      console.log($scope);
+  .controller('createManualOrderCtrl', [
+    '$scope',
+    'SupplierModel', 'UserModel', 'LoopBackAuth',
+    function($scope,
+             SupplierModel, UserModel, LoopBackAuth)
+    {
+      $scope.suppliers = [];
       $scope.stores = [];
 
 
@@ -17,14 +22,22 @@ angular.module('ShoppinPalApp')
        * This method will load the storesReport from api on view load
        */
       $scope.$on('$viewContentLoaded', function() {
-        SupplierModel.find({
-        })
-        .$promise.then(function(response) {
-            console.log(response[0].name);
-            $scope.stores = response;
-            $scope.selectedOrderStore = response[0].name;
-            $scope.selectedDeliverStore = response[0].name;
+        SupplierModel.find({})
+          .$promise.then(function(response) {
+            $scope.suppliers = response;
+            if($scope.suppliers && $scope.suppliers.length > 0) {
+              $scope.selectedSupplier = $scope.suppliers[0];
+            }
+
+            return UserModel.storeModels({id: LoopBackAuth.currentUserId})
+              .$promise.then(function(response){
+                $scope.stores = response;
+                if($scope.stores && $scope.stores.length > 0) {
+                  $scope.selectedStore = $scope.stores[0];
+                }
+              });
           });
       });
 
-    }]);
+    }
+  ]);
