6892a2fac5237b68613cf304a4adf2dc476d7969
Pulkit Singhal <pulkit@shoppinpal.com>
2015-07-15 08:49:37 -0500

    change state/status naming conventions
    ... checking this in first so that nobody does any wasted work, will be checking in the changes based on these conventions soon

--- client/app/scripts/shoppinpal-constants.js
+++ client/app/scripts/shoppinpal-constants.js
@@ -1,10 +1,12 @@
 angular.module('shoppinpal-constants', [])
-  .constant('REPORT_EMPTY', 'report_empty')
-  .constant('MANAGER_NEW_ORDERS', 'manager_new_orders')
-  .constant('MANAGER_IN_PROCESS', 'manager_in_process')
-  .constant('WAREHOUSE_FULFILL', 'warehouse_fulfill')
-  .constant('MANAGER_RECEIVE', 'manager_receive')
-  .constant('REPORT_COMPLETE', 'report_complete')
+  .constant('ReportModelStates', {
+    'REPORT_EMPTY': 'report_empty',
+    'MANAGER_NEW_ORDERS': 'manager_new_orders',
+    'MANAGER_IN_PROCESS': 'manager_in_process',
+    'WAREHOUSE_FULFILL': 'warehouse_fulfill',
+    'MANAGER_RECEIVE': 'manager_receive',
+    'REPORT_COMPLETE': 'report_complete'
+  })
   .constant('proxyUrl', '@@proxyUrl')
   .constant('vendClientId','@@vendClientId')
   .constant('vendAuthEndpoint', '@@vendAuthEndpoint')
