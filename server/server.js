a36a7a9d4424175d8df4446d7a7271eec5284be3
Pulkit Singhal <pulkit@shoppinpal.com>
2015-09-12 22:36:31 -0500

    add the errorHandler programmatically ( issue #171 )

--- server/server.js
+++ server/server.js
@@ -24,3 +24,32 @@ app.start = function() {
 if (require.main === module) {
   app.start();
 }
+
+/**
+ * Programmatically add the errorHandler because config.<env>.js is not supposed to be checked-in
+ * and its too painful to format: `'remoting': { 'errorHandler': 'handler': function(...){...} }`
+ * into a one liner for Jenkins configuration and sanity test it
+ * ... anytime there are minor changes to the code :(
+ *
+ * References:
+ *  > https://github.com/strongloop/loopback-faq-middleware#how-do-you-create-a-custom-error-message-for-all-errors
+ *
+ * @type {{handler: Function, disableStackTrace: boolean}}
+ */
+app.get('remoting').errorHandler = {
+  handler: function(error, req, res, next) {
+    /* Other options for namespace?
+     > 'strong-remoting:rest-adapter'
+     > 'server:middleware:errorHandler' */
+    var log = require('debug')('server:rest:errorHandler');
+    if (error instanceof Error) {
+      log('Error in %s %s: errorName=%s errorMessage=%s \n errorStack=%s',
+        req.method, req.url, error.name, error.message, error.stack);
+    }
+    else {
+      log(req.method, req.originalUrl, res.statusCode, error);
+    }
+    next(); /* let the default error handler (RestAdapter.errorHandler) run next */
+  },
+  disableStackTrace: true
+};
