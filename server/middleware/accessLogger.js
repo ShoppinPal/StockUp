54031f517da1231ddef98dba5a6623b3653e333f
Pulkit Singhal <pulkit@shoppinpal.com>
2015-09-13 19:28:38 -0500

    merge sessionLogger and responseTime middlewares (issue #171)

--- server/middleware/accessLogger.js
+++ server/middleware/accessLogger.js
@@ -8,14 +8,37 @@ module.exports = function() {
   return function accessLogger(req, res, next) {
     // enable audit log for API
     if (req.accessToken) {
-      log(req.method, req.originalUrl,
+      log('req', req.method, req.originalUrl,
         //'\n\t', 'userId:', req.accessToken.id,
         /*'\n\t',*/ 'token:', JSON.stringify(req.accessToken,null,0)
       );
     }
     else {
-      log(req.method, req.originalUrl);
+      log('req', req.method, req.originalUrl);
     }
+
+    // http://www.senchalabs.org/connect/responseTime.html
+    var start = new Date;
+    if (res._responseTime) {
+      return next();
+    }
+    res._responseTime = true;
+
+    // install a listener for when the response is finished
+    res.on('finish', function() { // the request was handled, print the log entry
+      var duration = new Date - start;
+      log('res', req.method, req.originalUrl, res.statusCode, duration + 'ms',
+        JSON.stringify({
+          lbHttpMethod:req.method,
+          lbUrl:req.originalUrl,
+          lbStatusCode:res.statusCode,
+          lbResponseTime:duration
+        },null,0)
+      );
+    });
+
+    // resume the routing pipeline,
+    // let other middleware to actually handle the request
     next();
   };
 };
