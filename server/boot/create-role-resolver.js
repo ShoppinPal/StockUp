cc0f31ddbb45abb679d955355fb0783b30dd303f
Pulkit Singhal <pulkit@shoppinpal.com>
2016-03-27 21:32:49 -0500

    issue #159 - `teamMember` role resovler should continue to behave as-is

--- server/boot/create-role-resolver.js
+++ server/boot/create-role-resolver.js
@@ -4,6 +4,8 @@ var path = require('path');
 var fileName = path.basename(__filename, '.js'); // gives the filename without the .js extension
 var log = require('debug')('boot:'+fileName);
 
+var _ = require('lodash');
+
 module.exports = function(app) {
   var Role = app.models.Role;
 
@@ -64,10 +66,11 @@ module.exports = function(app) {
       }
       else {
         var TeamModel = app.models.TeamModel;
+        var RoleMapping = app.models.RoleMapping;
         log('check if currentUserId:', currentUserId, '\n',
           'is in the team table for the given model\'s userId:', modelInstance.userId);
         TeamModel.count({
-          ownerId: modelInstance.userId,
+          ownerId: modelInstance.userId,  // only works for StoreConfigModel and SupplierModel which had its userId set to that of primary admin's ID at time of seeding
           memberId: currentUserId
         }, function(err, count) {
           if (err) {
@@ -75,18 +78,53 @@ module.exports = function(app) {
             return cb(null, false);
           }
 
-          log('is a team member? count > 0', (count > 0));
-          cb(null, count > 0); // true = is a team member
+          Role.findOne({
+            where: {name: 'manager'}
+          }, function(err, roleInstance) {
+            if (err) {
+              console.log(err);
+              return cb(null, false);
+            }
+            log('roleInstance:', roleInstance);
+
+            RoleMapping.find({
+              where: {
+                // https://github.com/strongloop/loopback-connector-mongodb/issues/128
+                // doesn't work because principalId is already stored as a string in mongo
+                // and changing the Model via boot script now:
+                // > RoleMapping.defineProperty('principalId', {type: ObjectID});
+                // won't change how data is already stored!
+                //principalId: currentUserId,
+                roleId: roleInstance.id
+              }
+            }, function(err, roleMappingInstances) {
+              if (err) {
+                console.log(err);
+                return cb(null, false);
+              }
+
+              //log('roleMappingInstances:', roleMappingInstances);
+              roleMappingInstances = _.filter(roleMappingInstances, function(roleMappingInstance){
+                return roleMappingInstance.principalId == currentUserId; // strict comparison with === fails
+              });
+              //log('roleMappingInstances:', roleMappingInstances);
+
+              log('is a team member? count > 0', (count > 0));
+              log('is a manager? roleMappingInstances.length > 0', (roleMappingInstances.length > 0));
+              cb(null, (count > 0 && roleMappingInstances.length > 0) ); // true = is a team member
+            });
+          });
+
         });
       }
     });
   });
 
   /*Role.registerResolver('$manager', function(role, context, callback) {
-  });
+      });
 
   Role.registerResolver('$warehouse', function(role, context, callback) {
-  });
+              });
 
   Role.registerResolver('$receiver', function(role, context, callback) {
   });*/
