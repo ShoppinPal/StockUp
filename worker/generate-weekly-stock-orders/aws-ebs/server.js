1788c76ed95e00bb45b4044575fa953a498c6b37
Pulkit Singhal <pulkit@shoppinpal.com>
2016-11-03 17:35:54 -0500

    minor tweaks

--- worker/generate-weekly-stock-orders/aws-ebs/server.js
+++ worker/generate-weekly-stock-orders/aws-ebs/server.js
@@ -5,35 +5,35 @@ var logger = require('tracer').console(); //var logger = console;
 http.createServer(function(req,res){
 
     req.on('data',function(data){
-        console.log('data', data);
         var payload, config;
         try {
             payload = JSON.parse(data.toString());
             config = {}; //TODO: worker specific config should come from somewhere?
-            console.log('payload', payload);
 
-            if(!payload || !validatePayload(payload)){
-                returnResponse(res,400,logger,'Invalid payload:', payload);
+            if (!payload || !validatePayload(payload)) {
+                returnResponse(res, 400, logger, 'Invalid payload: ' + JSON.stringify(payload));
             } else {
                 var generateWeeklyStockOrders = require('./generate-weekly-stock-orders');
                 generateWeeklyStockOrders.run(payload, config)
                   .then(function(){
-                      returnResponse(res,200,logger,'generated weekly stock orders successfully') ;
+                      returnResponse(res, 200, logger, 'generated weekly stock orders successfully') ;
+                  })
+                  .catch(function(error){
+                      logger.error(error);
+                      return;
                   });
             }
-        } catch(e){
-            returnResponse(res,400,logger,'Invalid json:', data.toString());
+        } catch(e) {
+            returnResponse(res, 400, logger, 'Invalid json: ' + data.toString());
         }
     });
 }).listen(process.env.PORT || 80);
 
-function returnResponse(httpResponse, status, logger, message){
-
-    var args = Array.prototype.slice.call(arguments);
-    if(status === 200){
-        logger.info(args.slice(3));//logger.info(message);
-    }else{
-        logger.error(args.slice(3));//logger.error(message);
+function returnResponse(httpResponse, status, logger, message) {
+    if (status === 200) {
+        logger.info(message);
+    } else{
+        logger.error(message);
     }
 
     httpResponse.writeHead(status);
