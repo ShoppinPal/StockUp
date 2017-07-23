45bf675149cce8b5238323c8230e5875a6971fa5
Kamal Khatwani <kamalkhatwani69@gmail.com>
2017-07-24 01:55:34 +0530

    Circle CI to run protractor tests

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,5 +3,6 @@ exports.config = {
   specs: ['./modules/auth/tests/*.js'],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 1000 * 60 * 10
-  }
+  },
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 };
