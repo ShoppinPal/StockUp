4f8a82fc9d3ea8271a343429fd2a7dfe5ad29988
Pulkit Singhal <pulkit@shoppinpal.com>
2017-08-07 14:47:32 -0700

    setup ESLint with warnings so that code can be cleaned up bit-by-bit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-empty": [
+            "warn"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ],
+        "no-console": [
+            "warn"
+        ],
+        "no-redeclare": [
+            "warn"
+        ],
+        "no-undef": [
+            "warn"
+        ],
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ],
+        "func-names": [
+            "warn",
+            "always"
+        ]
+    }
+};
