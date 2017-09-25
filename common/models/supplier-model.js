c4b13affb8b27799ab3ff1624ba53b0ee2fd728b
Kamal Khatwani <kamalkhatwani69@gmail.com>
2017-09-25 17:34:55 +0530

    Disable FFCC by setting isActive: false in its object. All others are set to true.

--- common/models/supplier-model.js
+++ common/models/supplier-model.js
@@ -1,20 +1,20 @@
 var Promise = require('bluebird');
 
 /*var path = require('path');
-var fileName = path.basename(__filename, '.js'); // gives the filename without the .js extension
-var log = require('./../lib/debug-extension')('common:models:'+fileName);*/
+ var fileName = path.basename(__filename, '.js'); // gives the filename without the .js extension
+ var log = require('./../lib/debug-extension')('common:models:'+fileName);*/
 
-module.exports = function(SupplierModel) {
+module.exports = function (SupplierModel) {
 
   // https://github.com/strongloop/loopback/issues/418
   // once a model is attached to the data source
-  SupplierModel.on('dataSourceAttached', function(obj){
+  SupplierModel.on('dataSourceAttached', function (obj) {
     // wrap the whole model in Promise
     // but we need to avoid 'validate' method
     SupplierModel = Promise.promisifyAll(
       SupplierModel,
       {
-        filter: function(name, func, target){
+        filter: function (name, func, target) {
           return !( name == 'validate');
         }
       }
@@ -27,19 +27,31 @@ module.exports = function(SupplierModel) {
       //log('find out any and all teams (ownerID) that the currentUser is a member of');
       var TeamModel = SupplierModel.app.models.TeamModel;
       TeamModel.find({where: {memberId: currentUser.id}}) // don't send a `return`, it will cause dual invocation of callback
-        .then(function(teamModels){
+        .then(function (teamModels) {
           //log('teamModels', teamModels);
           //log('TODO: for each team (ownerID) get the respective suppliers and return the results');
           if (teamModels && teamModels.length === 1) {
             // ASSUMPTION: (short-cut) currentUser only belongs to one team
-            SupplierModel.find({where: {userId: teamModels[0].ownerId}}, cb);
+            SupplierModel.find({
+              where: {
+                and: [
+                  {userId: teamModels[0].ownerId},
+                  {isActive: true}
+                ]
+              }
+            }, cb);
           }
           else {
             // checking TeamModel via count() of find() use extra cycles
             // which we can shortcut if we assume that hte user might be an $owner
             // if that is not the case, an empty result is provided, which is
             // almost as acceptable as an error
-            SupplierModel.find({where: {userId: currentUser.id}}, cb); //cb('user should be part of exactly one team');
+            SupplierModel.find({where: {
+              and: [
+                {userId: currentUser.id},
+                {isActive: true}
+              ]
+            }}, cb); //cb('user should be part of exactly one team');
           }
         });
     }
@@ -48,7 +60,7 @@ module.exports = function(SupplierModel) {
   SupplierModel.remoteMethod('listSuppliers', {
     accepts: [],
     http: {path: '/listSuppliers', verb: 'get'},
-    returns: {arg: 'suppliers', type: 'array', root:true}
+    returns: {arg: 'suppliers', type: 'array', root: true}
   });
 
 };
