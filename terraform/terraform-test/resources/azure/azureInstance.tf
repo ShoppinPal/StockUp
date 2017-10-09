f60ce21ae5cf63601f6d14bbf3372a4c734208f6
Yogesh Jadhav <yogesh@shoppinpal.com>
2017-10-09 23:18:07 +0530

    terraform setup updated with env variables

--- terraform/terraform-test/resources/azure/azureInstance.tf
+++ terraform/terraform-test/resources/azure/azureInstance.tf
@@ -1,6 +1,21 @@
 variable "Q_URL" {}
 variable "DLQ_URL" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_sqs_access_key" {}
+variable "aws_sqs_secret_key" {}
+variable "vend_client_id" {}
+variable "vend_client_secret" {}
 variable "admin_password" {}
+
+variable "vn_name" {}
+variable "subnet_name" {}
+variable "public_ip_name" {}
+variable "network_interface_name" {}
+variable "storage_account_name" {}
+variable "storage_container_name" {}
+variable "vm_name" {}
+
 # Resource Group
 resource "azurerm_resource_group" "test" {
   name     = "terraform_test"
@@ -53,7 +68,7 @@ resource "azurerm_network_interface" "test" {
 # Storage account
 # Note: Change storage account 'name' field if you want to recreate it in quick succession. Azure stores previous account name for some time!
 resource "azurerm_storage_account" "test" {
-  name                = "accsayogesh1"
+  name                = "${var.storage_account_name}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   location            = "eastus"
   account_type        = "Standard_LRS"
@@ -65,7 +80,7 @@ resource "azurerm_storage_account" "test" {
 
 # Storage Container
 resource "azurerm_storage_container" "test" {
-  name                  = "vhds"
+  name                  = "${var.storage_container_name}"
   resource_group_name   = "${azurerm_resource_group.test.name}"
   storage_account_name  = "${azurerm_storage_account.test.name}"
   container_access_type = "private"
@@ -74,11 +89,11 @@ resource "azurerm_storage_container" "test" {
 # Virtual Machine
 resource "azurerm_virtual_machine" "server" {
 
-  location  = "East US"
-  name      = "terraform-service"
+  location              = "East US"
+  name                  = "${var.vm_name}"
   resource_group_name   = "${azurerm_resource_group.test.name}"
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size      = "Standard_A0"
+  vm_size               = "Standard_A0"
 
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
@@ -117,7 +132,56 @@ resource "azurerm_virtual_machine" "server" {
       "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable' -y && ",
       "sudo apt-get update -y && ",
       "sudo apt-get install docker-ce -y && ",
-      "echo 'export AWS_SQS_URL=${var.Q_URL}' >> ~/.bashrc"
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && ",
+      "sudo chmod +x /usr/local/bin/docker-compose && ",
+      "docker-compose --version && ",
+      "echo 'export AWS_SQS_URL=${var.Q_URL}' >> ~/.bashrc && ",
+      "echo 'export AWS_ACCESS_KEY_ID=${var.aws_access_key}' >> ~/.bashrc && ",
+      "echo 'export AWS_SECRET_ACCESS_KEY=${var.aws_secret_key}' >> ~/.bashrc && ",
+      "echo 'export AWS_SQS_ACCESS_KEY_ID=${var.aws_sqs_access_key}' >> ~/.bashrc && ",
+      "echo 'export AWS_SQS_SECRET_ACCESS_KEY=${var.aws_sqs_secret_key}' >> ~/.bashrc && ",
+      "echo 'export VEND_CLIENT_ID=${var.vend_client_id}' >> ~/.bashrc && ",
+      "echo 'export VEND_CLIENT_SECRET=${var.vend_client_secret}' >> ~/.bashrc"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "terraform"
+      host     = "${azurerm_public_ip.test.ip_address}"
+      password = "${var.admin_password}"
+    }
+  }
+
+  provisioner "file" {
+    source = "~/.ssh/id_rsa"
+    destination = "~/.ssh/id_rsa"
+
+    connection {
+      type     = "ssh"
+      user     = "terraform"
+      host     = "${azurerm_public_ip.test.ip_address}"
+      password = "${var.admin_password}"
+    }
+  }
+
+  provisioner "file" {
+    source = "~/.ssh/id_rsa.pub"
+    destination = "~/.ssh/id_rsa.pub"
+
+    connection {
+      type     = "ssh"
+      user     = "terraform"
+      host     = "${azurerm_public_ip.test.ip_address}"
+      password = "${var.admin_password}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod -R 700 ~/.ssh/ && ",
+      "ssh-keyscan github.com >> ~/.ssh/known_hosts -y && ",
+      "git clone --recursive git@github.com:ShoppinPal/warehouse.git && cd warehouse && ",
+      "cp .env.example .env && cp worker.env.example worker.env"
     ]
 
     connection {
