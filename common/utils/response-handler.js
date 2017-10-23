893f36b653e343fc75d25b341e307311a6201026
Yogesh Jadhav <yogesh@shoppinpal.com>
2017-10-23 15:59:51 +0530

    Updated files to sp-json-logger

--- common/utils/response-handler.js
+++ common/utils/response-handler.js
@@ -2,32 +2,40 @@
 var q = require('q')
   , log = require('winston')
   , zlib = require('zlib');
+const logger = require('sp-json-logger');
 
 function decompressResponse (response, body) {
   var deferred = q.defer();
 
   if (response.headers['content-encoding'] === 'gzip') {
-    log.debug('response is gzipped. gunzipping...');
+    //log.debug('response is gzipped. gunzipping...');
+    logger.tag('Response Handler').debug({log: {message: 'response is gzipped. gunzipping...' }});
     zlib.gunzip(body, function(error, buffer){
-      log.debug('gunzipping now.');
+      //log.debug('gunzipping now.');
+      logger.tag('Response Handler').debug({log: {message: 'gunzipping... now..' }});
       if(error) {
-        log.error('error: ' + error);
+        //log.error('error: ' + error);
+        logger.error({err: error});
         deferred.reject(error);
       } else {
-        log.debug('buffer: ' + buffer.toString());
+        //log.debug('buffer: ' + buffer.toString());
+        logger.debug({log: {message: `buffer: ${buffer.toString()}` }});
         deferred.resolve(buffer.toString());
       }
     });
   }
   else if (response.headers['content-encoding'] === 'deflate') {
-    log.debug('response is deflated. inflating...');
+    //log.debug('response is deflated. inflating...');
+    logger.debug({log: {message: 'response is deflated. inflating...' }});
     zlib.inflate(body, function(error, buffer){
       log.debug('inflating now.');
       if(error) {
-        log.error(error);
+        //log.error(error);
+        logger.error({err: error});
         deferred.reject(error);
       } else {
-        log.debug(buffer.toString());
+        //log.debug(buffer.toString());
+        logger.debug({log: {message: buffer.toString() }});
         deferred.resolve(buffer.toString());
       }
     });
@@ -52,10 +60,12 @@ function decompressResponse (response, body) {
 
 var evaluateResponse = function (response, message) {
   if(response.statusCode === 200 || response.statusCode === 201) {
-    log.debug('Call succeeded. Resolving promise.');
+    //log.debug('Call succeeded. Resolving promise.');
+    logger.debug({log: {message: 'Call succeeded. Resolving promise.' }});
     return q(message);
   } else {
-    log.debug('Call failed. Rejecting promise. Status Code: ' + response.statusCode + '\n' + JSON.stringify(message));
+    //log.debug('Call failed. Rejecting promise. Status Code: ' + response.statusCode + '\n' + JSON.stringify(message));
+    logger.debug({log: {message: `Call failed. Rejecting promise. Status Code: ${response.statusCode}`, responseMessage: message }});
     return q.reject(message);
   }
 };
