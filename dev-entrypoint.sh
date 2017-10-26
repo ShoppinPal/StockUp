553b88ced44d37c64926ffb610d9a0b152a0d2cb
Yogesh Jadhav <yogesh@shoppinpal.com>
2017-10-26 19:40:54 +0530

    code clean up

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -56,14 +56,5 @@ _deploy_setup() {
         _deploy_setup ${NODE_ENV}
         #_generate_config ${NODE_ENV}
   fi
-  # Change uid and gid of node user so it matches ownership of current dir
-#   MAP_NODE_UID=$PWD
-#   uid=$(stat -c '%u' "$MAP_NODE_UID")
-#   gid=$(stat -c '%g' "$MAP_NODE_UID")
-#   usermod -u $uid node 2> /dev/null && {
-#     groupmod -g $gid node 2> /dev/null || usermod -a -G $gid node
-#   }
-#   chown -R node:node .
-#   exec gosu node "$@"
 
 exec "$@"
