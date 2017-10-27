05b028e3a9e2f5360da841f7c2c210344d4c7966
Kamal Khatwani <kamalkhatwani69@gmail.com>
2017-10-27 09:56:57 +0530

    Navigation UI for stuck orders, to give an idea

--- admin/src/app/_nav.ts
+++ admin/src/app/_nav.ts
@@ -1,5 +1,23 @@
 export const navigation = [
   {
+    title: true,
+    name: 'Orders'
+  },
+  {
+    name: 'Stuck Orders',
+    url: '',
+    icon: 'icon-close',
+    badge: {
+      variant: 'info',
+      text: '5'
+    }
+
+  },
+  {
+    title: true,
+    name: 'Settings'
+  },
+  {
     name: 'Stores',
     url: '/stores',
     icon: 'icon-location-pin'
