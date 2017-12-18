591f58e289fc0a267c1c6d1b019943a961343237
Bhushan Gadekar <bhushan@shoppinpal.com>
2017-12-18 15:54:50 +0530

    updated script for receiving terraform as an argument

--- getenv.js
+++ getenv.js
@@ -2,7 +2,7 @@
 
 var questions = require('./questions/qustions').getQustions();
 var inquirer = require('inquirer');
-var environment = process.env.npm_config_e ? process.env.npm_config_e : 'web';
+var environment = process.env.npm_config_e ? process.env.npm_config_e : 'terraform';
 var fs = require('fs');
 var obj = {};
 var text;
@@ -64,7 +64,7 @@ function generateEnv(data) {
                                         text += '\n SCHEME = ' + data[key][gKey];
                                         break;
                                     case 'external_ip':
-                                        text+='\n\n ##\
+                                        text += '\n\n ##\
                                         \n # To accommodate redirects, the code needs to know the external facing URL (FQDN or IP)\
                                         \n ##';
                                         text += '\n VM_EXTERNAL_IP = ' + data[key][gKey];
@@ -77,7 +77,7 @@ function generateEnv(data) {
                         }
                         break;
                     case 'aws':
-                        text+='\n\n # AWS account credentials\n'
+                        text += '\n\n # AWS account credentials\n'
                         for (var aKey in data[key]) {
                             if (data[key].hasOwnProperty(aKey)) {
                                 switch (aKey) {
@@ -88,7 +88,7 @@ function generateEnv(data) {
                                         text += '\n AWS_SECRET_ACCESS_KEY = ' + data[key][aKey];
                                         break;
                                     case 'aws_bucket':
-                                        text +='\n\n ## \n # A seed file contains information to populate the database. It contains: \n # - roles, usernames, passwords \n # - integration tokens gained via a successful oauth handshake, \n #   used to communicate with a point-of-sale (Vend) on behalf of retailers \n # - suppliers, outlets \n # - anything and everything which we cannot add via a UI for onboarding warehouse users. \n ##\n';
+                                        text += '\n\n ## \n # A seed file contains information to populate the database. It contains: \n # - roles, usernames, passwords \n # - integration tokens gained via a successful oauth handshake, \n #   used to communicate with a point-of-sale (Vend) on behalf of retailers \n # - suppliers, outlets \n # - anything and everything which we cannot add via a UI for onboarding warehouse users. \n ##\n';
                                         text += '\n AWS_BUCKET = ' + data[key][aKey];
                                         break;
                                     case 'aws_default_region':
@@ -102,7 +102,7 @@ function generateEnv(data) {
                         }
                         break;
                     case 'vend':
-                        text+='\n\n ##\
+                        text += '\n\n ##\
                         \n # Whomever is running warehouse, needs to be identified as a unique player by the point-of-sale (Vend)\
                         \n # To get these, you can:\
                         \n # - Register as a developer or Sign In: https://developers.vendhq.com/\
@@ -144,51 +144,66 @@ function generateEnv(data) {
 
 }
 
-
-if (environment === 'worker') {
-    text = '';
-    inquirer.prompt(questions.workerqs).then(answers => {
-        obj.answers = answers;
-        //obj.worker=answers;
-    }).then(() => {
-        inquirer.prompt(questions.endqs).then(endval => {
-            obj.end = endval;
-            generateEnv(obj);
+switch (environment) {
+    case 'worker':
+        text = '';
+        inquirer.prompt(questions.workerqs).then(answers => {
+            obj.answers = answers;
+            //obj.worker=answers;
+        }).then(() => {
+            inquirer.prompt(questions.endqs).then(endval => {
+                obj.end = endval;
+                generateEnv(obj);
+            });
         });
-    });
-} else if (environment === 'web') {
-    text = ' ## \n # Each worker takes care of a different task. \n # Sometimes workers are renamed, so in order to avoid code changes, \n # those worker names can be configured here, instead.\n # DO NOT TOUCH - unless you are a developer.\n ## \n';
-    text+='\n GENERATE_STOCK_ORDER_WORKER=generateStockOrderSeriallyWithPaging\
+        break;
+    case 'web':
+        text = ' ## \n # Each worker takes care of a different task. \n # Sometimes workers are renamed, so in order to avoid code changes, \n # those worker names can be configured here, instead.\n # DO NOT TOUCH - unless you are a developer.\n ## \n';
+        text += '\n GENERATE_STOCK_ORDER_WORKER=generateStockOrderSeriallyWithPaging\
     \n IMPORT_STOCK_ORDER_TO_POS=addProductsToVendConsignment\
     \n IMPORT_STOCK_ORDER_TO_WAREHOUSE=wh.order.import.cached\
     \n IMPORT_STOCK_ORDER_TO_WAREHOUSE_WITHOUT_SUPPLIER=wh.order.import.cached.excel.without.supplier\
     \n REMOVE_UNFULFILLED_PRODUCTS_WORKER=removeUnfulfilledProductsFromStockOrder\
     \n REMOVE_UNRECEIVED_PRODUCTS_WORKER=removeUnreceivedProductsFromStockOrder\
     \n STOCK_ORDER_WORKER=generateStockOrder\n';
-    inquirer.prompt(questions.generalQuestions).then(answers => {
-        obj.general = answers;
-    }).then(() => {
-        inquirer.prompt(questions.awsqs).then(awsvals => {
-            obj.aws = awsvals;
+        inquirer.prompt(questions.generalQuestions).then(answers => {
+            obj.general = answers;
         }).then(() => {
-            inquirer.prompt(questions.vendqs).then(vendvals => {
-                obj.vend = vendvals;
+            inquirer.prompt(questions.awsqs).then(awsvals => {
+                obj.aws = awsvals;
             }).then(() => {
-                inquirer.prompt(questions.oauthqs).then(oavals => {
-                    obj.oauth = oavals;
+                inquirer.prompt(questions.vendqs).then(vendvals => {
+                    obj.vend = vendvals;
                 }).then(() => {
-                    inquirer.prompt(questions.endqs).then(endval => {
-                        obj.end = endval;
-                        generateEnv(obj);
+                    inquirer.prompt(questions.oauthqs).then(oavals => {
+                        obj.oauth = oavals;
+                    }).then(() => {
+                        inquirer.prompt(questions.endqs).then(endval => {
+                            obj.end = endval;
+                            generateEnv(obj);
+                        });
                     });
                 });
             });
         });
-    });
-} else {
-    console.log('------------------------------------');
-    console.log('Oops ! An Invalid Option Entered, Please choose either web or worker as an environment');
-    console.log('------------------------------------');
+        break;
+    case 'terraform':
+        console.log('------------------------------------');
+        console.log('terraform');
+        console.log('------------------------------------');
+        break;
+    case 'worker2':
+        console.log('------------------------------------');
+        console.log('worker2');
+        console.log('------------------------------------');
+        break;
+
+    default:
+        console.log('------------------------------------');
+        console.log('Oops ! An Invalid Option Entered, Please choose correct option among : ');
+        console.log(' 1.terraform \n 2.web \n 3.worker \n 4.worker2');
+        console.log('------------------------------------');
+        break;
 }
 
 
