dc67eeef28cbbd8db4225dd0957d1e0082916fa9
Shrey Gupta <connectwithshrey@gmail.com>
2019-06-01 22:38:59 +0530

    feat(Fulfill And Recieve): Barcode Scanning for StockOrder Fulfillment And Recieve
    - Barcode Scanning for StockOrder Fulfillment And Recieve
    - Added a
    KeyUp debounce Directive
    - Added Logic to Manipulate Quanities
    
    BREAKING CHANGE: Moved appAutoFocus direcdtive's decleration to shared
    module

---
+++ admin/src/app/shared/directives/DebounceKeyUp/debounce-key-up.directive.ts
@@ -0,0 +1,33 @@
+/**
+ * https://coryrylan.com/blog/creating-a-custom-debounce-click-directive-in-angular
+ */
+import {Directive, EventEmitter, HostListener, OnDestroy, OnInit, Output, Input} from '@angular/core';
+import {Subject, Subscription} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
+@Directive({
+  selector: '[appDebounceKeyUp]'
+})
+export class DebounceKeyUpDirective implements OnInit, OnDestroy {
+  @Input() debounceTime = 1000;
+  @Output('appDebounceKeyUp') debounceKeyUp = new EventEmitter();
+  private keyUp = new Subject();
+  private subscription: Subscription;
+  constructor() { }
+
+  ngOnInit() {
+    this.subscription = this.keyUp.pipe(
+        debounceTime(this.debounceTime)
+    ).subscribe(e => this.debounceKeyUp.emit(e))
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  @HostListener('keyup', ['$event'])
+  keyUpEvent(event) {
+    event.stopPropagation();
+    this.keyUp.next(event)
+  }
+}
