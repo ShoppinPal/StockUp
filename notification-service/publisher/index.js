681b49396f3daefd07cfa1442e33ca7a55c7854f
Shrey Gupta <connectwithshrey@gmail.com>
2019-07-05 14:45:16 +0530

    Notification Service Added

---
+++ notification-service/publisher/index.js
@@ -0,0 +1,33 @@
+'use strict';
+const path = require('path');
+const fileName = path.basename(__filename, '.js'); // gives the filename without the .js extension
+let utils = require('../utils/utils');
+const logger = require('sp-json-logger')({fileName: + 'notification-service:publisher' + fileName});
+
+module.exports = (app) => {
+    /**
+     * Worker will call this endpoint once it finishes its task.
+     * We will then publish the received message to redis.
+     * POST /publish
+     */
+    app.post('/publish', function (req, res) {
+        if (!req.body) {
+            logger.error('Empty post request received for /publish from worker');
+            res.status(400).send('POST body is missing');
+        }
+        let payload = req.body; // add object property validations!
+        utils.publishToRedis(app, payload)
+            .then(() => {
+                res.send({
+                    success: true,
+                    message: 'worker payload published to redis'
+                });
+            })
+            .catch(error => {
+                res.status(500).send({
+                    success: false,
+                    error
+                });
+            });
+    });
+};
---
+++ notification-service/utils/utils.js
@@ -0,0 +1,106 @@
+'use strict';
+const logger = require('sp-json-logger')();
+const MESSAGE_FOR_CLIENT = 'MESSAGE_FOR_CLIENT';
+const MESSAGE_FOR_API = 'MESSAGE_FOR_API';
+let cleanupSSEMap = (sseMap, keyToDelete) => {
+    setTimeout(() => {
+        if (sseMap[keyToDelete]) {
+            sseMap[keyToDelete].res.end();
+            delete sseMap[keyToDelete];
+            logger.debug(`sseMap cleaned up for key: ${keyToDelete}`);
+        }
+        else {
+            logger.debug(`could not clear sseMap as recipient: ${keyToDelete} was not found`);
+        }
+    }, process.env.MAX_API_CALL_IDLE_TIME * 1000);
+};
+
+let cleanUsersIfInactive = (sseUsers) => {
+    setInterval(function() {
+        let keys = Object.keys(sseUsers);
+        let totalKeys = keys.length;
+        let maxIdleTime = 1000 * 60 * process.env.MAX_USER_IDLE_TIME;
+        let currenTime = new Date();
+
+        logger.debug('cleaning inactive user streams');
+    
+        for(let i = 0; i < totalKeys; i++) {
+            
+            if(currenTime - sseUsers[keys[i]].timeStamp >= maxIdleTime) {
+                sseUsers[keys[i]].res.end();
+                delete sseUsers[keys[i]];
+            }
+        }
+    }, 1000 * 60);
+};
+
+function publishToRedis(app, payload) {
+    return Promise.resolve()
+        .then(() => {
+            if (app.redis && app.redis.workerPublisher) {
+                let stringifiedPayload = JSON.stringify(payload);
+
+                app.redis.workerPublisher.publish(
+                    process.env.REDIS_WORKER_CHANNEL,
+                    stringifiedPayload,
+                    (err) => {
+                        if (err) {
+                            return Promise.reject(err);
+                        }
+                        logger.debug({
+                            message: 'published worker data to redis'
+                        });
+                        return Promise.resolve();
+                    });
+            }
+            else {
+                let error = new Error('Could not publish to redis server as connection to redis is either down or corrupted');
+                logger.error({
+                    error,
+                    message: 'could not publish data to redis'
+                });
+                return Promise.reject(error);
+            }
+        });
+}
+
+function sendSSEOutput(sseMapObject, recipient, eventType, status, data, messageFor) {
+    try{
+        logger.debug({
+            message: `sending notification to ${messageFor}: ${recipient} for event: ${eventType}`,
+            functionName: 'redis-subscription:listener'
+        });
+        if (sseMapObject[recipient]) {
+            let sse = sseMapObject[recipient].sse;
+            sse.send({ eventType, data , status});
+            logger.debug(`SSE event sent to ${messageFor}`);
+
+            if (messageFor === MESSAGE_FOR_CLIENT) {
+                // update the timestamp to track activity
+                sseMapObject[recipient].timeStamp = new Date();
+                logger.debug(`Updated timestamp for userId: ${recipient}`);
+            }
+        }
+        else {
+            logger.debug(`Could not find recipient ${recipient} in sseMapObject`);
+        }
+    }
+    catch(error) {
+        logger.error({
+            error,
+            message: 'Error occurred while sending SSE event',
+            functionName: 'redis-subscription:listener'
+        });
+    }
+}
+
+module.exports = {
+    cleanupSSEMap,
+    cleanUsersIfInactive,
+    publishToRedis,
+    sendSSEOutput,
+    constants: {
+        MESSAGE_FOR_CLIENT,
+        MESSAGE_FOR_API
+    }
+}
