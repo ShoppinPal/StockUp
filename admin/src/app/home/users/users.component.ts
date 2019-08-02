1b85529bd49696af8ef698ad958a2ce3de9079e7
Shrey Gupta <connectwithshrey@gmail.com>
2019-08-02 19:41:15 +0530

    Add Virtual User

--- admin/src/app/home/users/users.component.ts
+++ admin/src/app/home/users/users.component.ts
@@ -4,6 +4,7 @@ import {OrgModelApi} from "../../shared/lb-sdk/services/custom/OrgModel";
 import {ToastrService} from 'ngx-toastr';
 import {Observable, combineLatest} from 'rxjs';
 import {UserProfileService} from "../../shared/services/user-profile.service";
+import {UserModel} from '../../shared/lb-sdk/models';
 
 @Component({
   selector: 'app-users',
@@ -30,6 +31,7 @@ export class UsersComponent implements OnInit {
   public foundUser: boolean = false;
   public searchedUser: Array<any>;
   public maxPageDisplay: number = 7;
+  public newUserEmail: string;
 
   ngOnInit() {
     this.userProfile = this._userProfileService.getProfileData();
@@ -94,4 +96,24 @@ export class UsersComponent implements OnInit {
     this._router.navigate(['users/user-details/' + userId]);
   }
 
+  createVirtualUser() {
+    this.loading = true;
+    this.orgModelApi.createUserModels(this.userProfile.orgModelId, {
+      email: this.newUserEmail,
+      password: Math.random().toString(36).slice(-8),
+      virtual: true
+    })
+      .subscribe(user => {
+      this.newUserEmail = '';
+      this.loading = false;
+      this.currentPage = 1;
+      this.fetchUsers();
+      this.toastr.success('Virtual user created successfully');
+    }, error => {
+        this.loading = false;
+        console.error(error);
+      this.toastr.error('Virtual user creation failed');
+
+    })
+  }
 }
