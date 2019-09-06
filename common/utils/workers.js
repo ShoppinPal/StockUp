0864901e04ea2664e2316b7ad2259e742b4920b5
Kamal Khatwani <kamalkhatwani69@gmail.com>
2019-09-07 04:08:04 +0530

    Removed unused backend code

--- common/utils/workers.js
+++ common/utils/workers.js
@@ -4,93 +4,97 @@ var _ = require('underscore');
 
 var path = require('path');
 var fileName = path.basename(__filename, '.js'); // gives the filename without the .js extension
-var log = require('./../lib/debug-extension')('common:models:' + fileName);
+const logger = require('sp-json-logger')({fileName: 'common:models:' + fileName});
 
 var app = require('./../../server/server'); //https://loopback.io/doc/en/lb2/Working-with-LoopBack-objects.html
 
 var sendPayLoad = function (payload) {
-  var AWS = require('aws-sdk');
-  var sqs = new AWS.SQS({
-    region: app.get('awsQueueRegion'),
-    accessKeyId: app.get('awsQueueAccessKeyId'),
-    secretAccessKey: app.get('awsQueueSecretAccessKey')
-  });
-  //var msg = { payload: 'a message' };
-  var sqsParams = {
-    MessageBody: JSON.stringify(payload),
-    QueueUrl: app.get('awsQueueUrl2')
-  };
-  var sendMessageAsync = Promise.promisify(sqs.sendMessage, sqs);
-
-  return sendMessageAsync(sqsParams)
-    .then(successHandler)
-    .then(function (data) {
-      log.debug('Received this worker task info', JSON.stringify(data, null, 2));
-      /*SQS sample response:
-       {
-       "ResponseMetadata": {
-       "RequestId": "aaa"
-       },
-       "MD5OfMessageBody": "bbb",
-       "MessageId": "ccc"
-       }*/
-      return Promise.resolve(data);
-    })
-    .catch(ClientError, function (e) {
-      console.log(e);
-      var message = e.response.body; //TODO: TypeError: Cannot read property 'body' of undefined
-      if (_.isObject(message)) {
-        message = JSON.stringify(message, null, 2);
-      }
-      console.error('A ClientError happened: \n'
-        + e.statusCode + ' ' + message + '\n'
-        /*+ JSON.stringify(e.response.headers,null,2)
-         + JSON.stringify(e,null,2)*/
-      );
-      // TODO: add retry logic?
-      return Promise.reject(e.statusCode + ' ' + message); // TODO: throw unknown errors but reject well known errors?
-      // cb(e.statusCode + ' ' + message);
-    })
-    .catch(function (e) {
-      console.error('report-model.js - generateStockOrderReportForManager - An unexpected error occurred: ', e);
-      //throw e; // TODO: throw unknown errors but reject well known errors?
-      return Promise.reject(e);
-      // cb(e);
+    var AWS = require('aws-sdk');
+    var sqs = new AWS.SQS({
+        region: app.get('awsQueueRegion'),
+        accessKeyId: app.get('awsQueueAccessKeyId'),
+        secretAccessKey: app.get('awsQueueSecretAccessKey')
     });
+    //var msg = { payload: 'a message' };
+    var sqsParams = {
+        MessageBody: JSON.stringify(payload),
+        QueueUrl: app.get('awsQueueUrl2')
+    };
+    var sendMessageAsync = Promise.promisify(sqs.sendMessage, sqs);
+
+    return sendMessageAsync(sqsParams)
+        .then(successHandler)
+        .then(function (data) {
+            logger.debug({
+                message: 'Received this worker task info',
+                data,
+                functionName: 'sendPayLoad'
+            });
+            /*SQS sample response:
+             {
+             "ResponseMetadata": {
+             "RequestId": "aaa"
+             },
+             "MD5OfMessageBody": "bbb",
+             "MessageId": "ccc"
+             }*/
+            return Promise.resolve(data);
+        })
+        .catch(ClientError, function (e) {
+            console.log(e);
+            var message = e.response.body; //TODO: TypeError: Cannot read property 'body' of undefined
+            if (_.isObject(message)) {
+                message = JSON.stringify(message, null, 2);
+            }
+            console.error('A ClientError happened: \n'
+                + e.statusCode + ' ' + message + '\n'
+                /*+ JSON.stringify(e.response.headers,null,2)
+                 + JSON.stringify(e,null,2)*/
+            );
+            // TODO: add retry logic?
+            return Promise.reject(e.statusCode + ' ' + message); // TODO: throw unknown errors but reject well known errors?
+            // cb(e.statusCode + ' ' + message);
+        })
+        .catch(function (e) {
+            console.error('report-model.js - generateStockOrderReportForManager - An unexpected error occurred: ', e);
+            //throw e; // TODO: throw unknown errors but reject well known errors?
+            return Promise.reject(e);
+            // cb(e);
+        });
 };
 
 var ClientError = function ClientError(e) {
-  return e.statusCode>=400 && e.statusCode<500;
+    return e.statusCode>=400 && e.statusCode<500;
 };
 var successHandler = function (response) {
-  if (_.isArray(response)) {
-    console.log('response is an array');
-  }
-  else if (_.isObject(response)) {
-    console.log('response is an object');
-    return Promise.resolve(response);
-  }
-  else if (_.isString(response)) {
-    console.log('response is a string');
-    try {
-      var responseObject = JSON.parse(response);
-      //console.log(responseObject);
-      return Promise.resolve(responseObject);
+    if (_.isArray(response)) {
+        console.log('response is an array');
     }
-    catch (error) {
-      console.error('caught an error: ', error);
-      throw error;
+    else if (_.isObject(response)) {
+        console.log('response is an object');
+        return Promise.resolve(response);
     }
-  }
-  else {
-    console.log(response);
-  }
+    else if (_.isString(response)) {
+        console.log('response is a string');
+        try {
+            var responseObject = JSON.parse(response);
+            //console.log(responseObject);
+            return Promise.resolve(responseObject);
+        }
+        catch (error) {
+            console.error('caught an error: ', error);
+            throw error;
+        }
+    }
+    else {
+        console.log(response);
+    }
 };
 
 module.exports = {
     sendPayLoad: sendPayLoad,
     messageFor: {
-      MESSAGE_FOR_CLIENT: 'MESSAGE_FOR_CLIENT',
-      MESSAGE_FOR_API: 'MESSAGE_FOR_API'
+        MESSAGE_FOR_CLIENT: 'MESSAGE_FOR_CLIENT',
+        MESSAGE_FOR_API: 'MESSAGE_FOR_API'
     }
 };
