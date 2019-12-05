41041d6ecba4cf9706c472b39e2cc888ad46a728
panic-coder <androidshubham@gmail.com>
2019-12-05 12:37:27 +0530

    fix search on enter in stock order complete

--- admin/src/app/home/orders/stock-orders/complete/complete.component.ts
+++ admin/src/app/home/orders/stock-orders/complete/complete.component.ts
@@ -137,7 +137,7 @@ export class CompleteComponent implements OnInit {
   }
 
    keyUpEvent(event, searchSKUText) {
-    if(event.keyCode == '13') {
+    if(event.keyCode == '13' && !this.enableBarcode) {
       this.searchProductBySku(searchSKUText)
     }
   }
