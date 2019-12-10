4282baeb240b60f02b503a6f0e9d609867c8cd45
panic-coder <androidshubham@gmail.com>
2019-12-10 18:16:54 +0530

    add validations in signup form

--- admin/src/app/signup/signup.component.ts
+++ admin/src/app/signup/signup.component.ts
@@ -5,6 +5,9 @@ import {UserModelApi} from "../shared/lb-sdk/services/custom/UserModel";
 import {LoopBackConfig} from "../shared/lb-sdk/lb.config";
 import {environment} from "../../environments/environment";
 import {flatMap} from "rxjs/operators";
+import {ToastrService} from 'ngx-toastr';
+import Utils from '../shared/constants/utils';
+
 
 @Component({
   selector: 'app-signup',
@@ -16,7 +19,7 @@ export class SignupComponent implements OnInit {
   private user: UserModel = new UserModel();
   public loading = false;
 
-  constructor(private userModelApi: UserModelApi, private _router: Router, private _route: ActivatedRoute) {
+  constructor(private userModelApi: UserModelApi, private _router: Router, private _route: ActivatedRoute, private toastr: ToastrService) {
     LoopBackConfig.setBaseURL(environment.BASE_URL);
     LoopBackConfig.setApiVersion(environment.API_VERSION);
   }
@@ -46,22 +49,49 @@ export class SignupComponent implements OnInit {
   }
 
   private signup(): void {
-    this.loading = true;
-    this.userModelApi.signup(this.user).pipe(flatMap((data: any) => {
-      return this.userModelApi.login(this.user);
-    }))
-      .subscribe((token: AccessToken) => {
-        this._router.navigate(['/connect']);
-      }, err => {
-        this.loading = false;
-      });
+    try {
+      if (this.user.orgName === undefined || this.user.orgName === null || this.user.orgName === '') {
+        throw new Error('Invalid organisation name');
+      }
+      if (this.user.email === undefined || this.user.email === null || this.user.email === '') {
+        throw new Error('Invalid email');
+      }
+      if (this.user.password === undefined || this.user.password === null || this.user.password === '') {
+        throw new Error('Invalid password');
+      }
+      if (this.user.confirmPassword === undefined || this.user.confirmPassword === null || this.user.confirmPassword === '') {
+        throw new Error('Invalid confirm password');
+      }
+      var validateEmail = Utils.singleValidateEmail((this.user.email).trim());
+      if(!validateEmail) {
+        throw new Error('Invalid email');
+      }
+
+      var validatePassword = Utils.validatePassword((this.user.password).trim());
+      if(!validatePassword) {
+        throw new Error('Minimum 6 characters required in password');
+      }
+      if(this.user.password !== this.user.confirmPassword) {
+        throw new Error('New password and confirm password must match');
+      }
+      this.loading = true;
+      this.userModelApi.signup(this.user).pipe(flatMap((data: any) => {
+        return this.userModelApi.login(this.user);
+      }))
+        .subscribe((token: AccessToken) => {
+          this._router.navigate(['/connect']);
+        }, err => {
+          this.loading = false;
+        });
+    } catch(error) {
+        this.toastr.error(error);
+    }
   }
 
   onKey(event) {
-    // console.log("event.keyCode : ",event.keyCode);
-    if(event.keyCode == '13') {
+    if(event.keyCode === '13' && (this.user.password !== undefined && this.user.password !== null && this.user.confirmPassword !== undefined && this.user.confirmPassword !== null)) {
       this.signup();
-    }
+    } 
   }
 
 }
