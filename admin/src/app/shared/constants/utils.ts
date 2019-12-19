6a9a45d2bb83c3a56f3eacd1d9ac81c38d60c831
panic-coder <androidshubham@gmail.com>
2019-12-19 15:11:31 +0530

    fix review | assign max password length to a variable

--- admin/src/app/shared/constants/utils.ts
+++ admin/src/app/shared/constants/utils.ts
@@ -1,4 +1,7 @@
 export default class Utils {
+
+  static maxLengthForPassword = 6;
+
   static validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email instanceof Array) {
@@ -20,7 +23,7 @@ export default class Utils {
   }
 
   static validatePassword(password) {
-    var re = /^.{6,}$/;
+    var re = new RegExp(`^.{${this.maxLengthForPassword},}$`);
     return re.test(String(password));
   }
 
