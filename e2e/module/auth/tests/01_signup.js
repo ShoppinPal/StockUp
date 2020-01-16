4f2a881b5b9f6f76f7889ba912805c391aacab37
khushbu Pathe <k.pathe96@gmail.com>
2020-01-16 14:09:50 +0530

    Added console log

--- e2e/module/auth/tests/01_signup.js
+++ e2e/module/auth/tests/01_signup.js
@@ -3,14 +3,19 @@ var authData = require('../testData/auth.td.js');
 
 describe('Signup module for signing up new users', function () {
 var authFunctions = new AuthFunctions();
-//var authData = new AuthData();
 // --------------------------------User login assertion with unregistered user------------------------------------
+    browser.ignoreSynchronization = true;
+    it('url verifications', function() {
+    // browser.get('https://staging.stockup.online/');
+    var baseUrl = browser.baseUrl;
+    var testUrl =  baseUrl + '/v2/#/login';
+    browser.get(testUrl);
+    browser.getCurrentUrl().then(function(url){
+        console.log("Web page url is : " +url )
+    })
+    });
     it('should assert user login and not allow unregistered user to login', function(){
-        browser.ignoreSynchronization = true;
-        var baseUrl = browser.baseUrl;
-        var testUrl =  baseUrl + '/v2/#/login';
-        browser.get(testUrl);
-        //browser.get('https://staging.stockup.online/');
+        console.log('On stockup page');
         authFunctions.login(authData);
         browser.sleep(5000);
     });
