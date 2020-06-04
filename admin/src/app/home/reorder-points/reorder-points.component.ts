df878731e6c6e4d0e011279b65aaccd958552f75
Kamal Khatwani <kamalkhatwani69@gmail.com>
2020-06-04 20:13:28 +0530

    UI for reorder points multiplier

--- admin/src/app/home/reorder-points/reorder-points.component.ts
+++ admin/src/app/home/reorder-points/reorder-points.component.ts
@@ -3,6 +3,8 @@ import {UserProfileService} from "../../shared/services/user-profile.service";
 import {ToastrService} from "ngx-toastr";
 import {OrgModelApi} from "../../shared/lb-sdk/services/custom/OrgModel";
 import {UserModelApi} from "../../shared/lb-sdk/services/custom/UserModel";
+import {FileUploader} from 'ng2-file-upload';
+import {LoopBackConfig, LoopBackAuth} from "../../shared/lb-sdk";
 
 @Component({
   selector: 'app-reorder-points',
@@ -15,15 +17,33 @@ export class ReorderPointsComponent implements OnInit {
   public loading: boolean = false;
   public salesDateRange: number;
   public stockUpReorderPoints: boolean;
+  public uploader: FileUploader;
+  public multiplier: number;
 
   constructor(private orgModelApi: OrgModelApi,
               private userModelApi: UserModelApi,
               private toastr: ToastrService,
-              private _userProfileService: UserProfileService) {
+              private _userProfileService: UserProfileService,
+              private auth: LoopBackAuth) {
   }
 
   ngOnInit() {
     this.userProfile = this._userProfileService.getProfileData();
+
+    /**
+     * Reorder points multiplier file upload
+     * @type {string}
+     */
+    let reorderPointsMultiplerUrl: string = LoopBackConfig.getPath() + "/" + LoopBackConfig.getApiVersion() +
+      "/OrgModels/" + this.userProfile.orgModelId + "/uploadReorderPointsMultiplier";
+    this.uploader = new FileUploader({
+      url: reorderPointsMultiplerUrl,
+      autoUpload: false,
+      authToken: this.auth.getAccessTokenId(),
+      removeAfterUpload: true
+    });
+
+
     this.userModelApi.getOrgModel(this.userProfile.userId)
       .subscribe((data: any) => {
           this.salesDateRange = data.salesDateRangeInDays;
@@ -48,6 +68,31 @@ export class ReorderPointsComponent implements OnInit {
         console.log(err);
         this.toastr.error('Error saving settings');
       });
+  }
+
+  uploadMinMaxFile() {
+    this.loading = true;
+    console.log('uploading file...', this.uploader);
+    this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+      form.append('multiplier', this.multiplier);
+    };
+    this.uploader.uploadAll();
+    this.uploader.onSuccessItem = (item: any, response: any, status: number, headers: any): any => {
+      this.loading = false;
+      if (response && response.result) {
+        this.toastr.success(response.result);
+      }
+      else {
+        this.toastr.success('Successfully updated multiplier with SKUs');
+      }
+    };
+    this.uploader.onErrorItem = (item: any, response: any, status: number, headers: any): any => {
+      this.loading = false;
+      console.log('error uploading file');
+      console.log('response', response);
+      console.log('status', status);
+      this.toastr.error(response);
+    };
   }
 
 }
