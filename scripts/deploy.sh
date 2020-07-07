6a6d3f971060045051db5ffe8355ff0def09e0df
Rishabh <rishabh@shoppinpal.com>
2020-07-07 12:08:41 +0530

    migrated images from DockerHub to GCR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,13 +12,13 @@ _BUILD_TAG="v$_VERSION-b$_SHORT_SHA"
 ./scripts/build/tag ${_RAW_VERSION} ${_BUILD_TAG}
 if [ "${_BRANCH_NAME}" == "master" ]; then
 scripts/build/tag ${_BUILD_TAG} "production-${_BUILD_TAG}"
-docker push shoppinpal/warehouse:production-${_BUILD_TAG}
+docker push gcr.io/warehouse-build/warehouse:production-${_BUILD_TAG}
 fi
 if [ "${_BRANCH_NAME}" == "develop" ]; then
 scripts/build/tag ${_BUILD_TAG} "dev-${_BUILD_TAG}"
-docker push shoppinpal/warehouse:dev-${_BUILD_TAG}
+docker push gcr.io/warehouse-build/warehouse:dev-${_BUILD_TAG}
 fi
 if [[ "${_BRANCH_NAME}" =~ ^release.* ]]; then
 scripts/build/tag ${_BUILD_TAG} "staging-${_BUILD_TAG}"
-docker push shoppinpal/warehouse:staging-${_BUILD_TAG}
+docker push gcr.io/warehouse-build/warehouse:staging-${_BUILD_TAG}
 fi
